2381dd14cfc1b7c2d19053773a54cbc7fe37f3f4
Rafael Bonaldi <rafael.simoes@supermix.com.br>
2024-04-25 15:51:54 -0300

    Ajuste retorno do metodo

--- Application/CriptoMoedaService.cs
+++ Application/CriptoMoedaService.cs
@@ -19,22 +19,7 @@ namespace Application
                 throw new Exception("Sigla escolhida não é valida.");
             }
 
-            var retorno = await mercadoBitcoinAdapter.ObterDadosNegociacoesDoDiaAsync(siglaMoeda);
-
-            if(retorno.MenorPrecoOfertado > retorno.MaiorPrecoOfertado)
-            {
-                // Correção: Verificar se PrecoDivisao é diferente de zero antes de dividir
-                if (retorno.PrecoDivisao != 0)
-                {
-                    retorno.PrecoFinal = retorno.MaiorPreco / retorno.PrecoDivisao;
-                }
-                else
-                {
-                    retorno.PrecoFinal = retorno.MaiorPreco; // Ou aplicar outra lógica conforme regra de negócio
-                }
-            }
-
-            return retorno;
+            return await mercadoBitcoinAdapter.ObterDadosNegociacoesDoDiaAsync(siglaMoeda);
         }
     }
 }
