e2ac6aed6603b1333a42179c95aecad7b71ba164
Rafael Bonaldi <rafael.simoes@supermix.com.br>
2024-05-02 08:45:29 -0300

    ajuste cors

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod());
+});
+
 //Service
 builder.Services.AddScoped<ICriptoMoedaService, CriptoMoedaService>();
 
@@ -31,6 +39,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins");
 
 app.UseAuthorization();
 
